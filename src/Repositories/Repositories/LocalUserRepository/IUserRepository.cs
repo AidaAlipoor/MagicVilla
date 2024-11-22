4beae179156e36431268be29637f4a31429e9648
hp <hp@AIDA>
2024-11-23 00:26:44 +0330

    adding UserRepository and interface

---
+++ src/Repositories/Repositories/LocalUserRepository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Business.Dtos.LocalUserDto;
+using Business.ViewModels;
+using DataAccess.Entities;
+
+namespace Business.Repositories.LocalUserRepository
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseViewModel> Login(LoginRequestDto loginRequestDto);
+        Task<LocalUser> Register(RegistrationRequestDto registrationRequestDto);
+    }
+}
---
+++ src/Repositories/Repositories/LocalUserRepository/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Business.Dtos.LocalUserDto;
+using Business.Repositories.ParentRepository;
+using Business.ViewModels;
+using DataAccess;
+using DataAccess.Entities;
+
+namespace Business.Repositories.LocalUserRepository
+{
+    internal class UserRepository : Repository<LocalUser>, IUserRepository
+    {
+        private readonly IMapper _mapper;
+
+        public UserRepository(ApplicationDbContext dbContext, IMapper mapper)
+            : base(dbContext) => _mapper = mapper;
+
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseViewModel> Login(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
